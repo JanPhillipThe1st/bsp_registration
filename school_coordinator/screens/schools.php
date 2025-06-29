a8137d4ede84f05fbbeb718301e98c21739f9da5
Jan Phillip <juntado38@gmail.com>
2025-06-29 23:37:31 +0800

    School coordinator

--- school_coordinator/screens/schools.php
+++ school_coordinator/screens/schools.php
@@ -222,7 +222,7 @@ if (!isset($_SESSION["username"])) {
                         $("<td>"+school.contact+"</td>")
                     )
                     .append(
-                        $("<td>"+school.coordinator+"</td>")
+                        $("<td></td>")
                     ).click(()=>{
 
                         $.post("../ajax.php",{action:"get_school_coordinator",ID:school.school_coordinator_id},(school_coordinator_response)=>{
@@ -268,7 +268,7 @@ if (!isset($_SESSION["username"])) {
                         $("<td style='border:1px solid black'>"+school.contact+"</td>")
                     )
                     .append(
-                        $("<td style='border:1px solid black'>"+school.coordinator+"</td>")
+                        $("<td style='border:1px solid black'></td>")
                     )
                 );
             });
