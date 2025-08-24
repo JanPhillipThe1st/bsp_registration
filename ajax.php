f0f77fb45abfafa973051416173ede2617eb00cf
Jan Phillip <juntado38@gmail.com>
2025-08-24 17:25:52 +0800

    a

--- ajax.php
+++ ajax.php
@@ -1088,7 +1088,7 @@ if($action == 'reset_password'){
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'mail.bsp-zds.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'bspmailer@bsp-zds.com';                     //SMTP username
         $mail->Password   = 'RainorShine2!';                               //SMTP password
